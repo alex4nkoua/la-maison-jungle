41fbc5838dd84f1e192db48214e8fbcd4e8d6e9c
Alexandre Nkoua <alexnk4code@gmail.com>
2023-10-10 18:25:45 +0200

    ajout du css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,5 @@
+import "../styles/Cart.css";
+
 function Cart() {
     const panier = ["un monstera", "un lierre", "un bouquet de fleur"];
 
@@ -7,12 +9,12 @@ function Cart() {
 
     return (
         <>
-            <ul>
+            <ul className="lmj-cart">
                 <li>{panier[0]} : {prixPlante1}€</li>
                 <li>{panier[1]} : {prixPlante2}€</li>
                 <li>{panier[1]} : {prixPlante3}€</li>
             </ul>
-            <span>Le total du panier est de {prixPlante1 + prixPlante2 + prixPlante3}€</span>
+            <span>Le total : {prixPlante1 + prixPlante2 + prixPlante3}€</span>
         </>
     )
 }
